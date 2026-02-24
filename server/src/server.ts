3427f3c0a001bd4ce46ac6ae7d7fe40c0e83b813
Kenneth Legesi <sklegesi@gmail.com>
2026-02-24 23:00:43 +0300

    feat(server): add account balance and transaction endpoints

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import Fastify from 'fastify'
 
 import { registerHealthRoutes } from './routes/health'
+import { registerAccountRoutes } from './routes/accounts'
 import { registerPaymentRoutes } from './routes/payments'
 import { registerXenteWebhookRoutes } from './routes/xente'
 
@@ -8,6 +9,7 @@ export default function buildServer() {
   const server = Fastify({ logger: true })
 
   server.register(registerHealthRoutes, { prefix: '/health' })
+  server.register(registerAccountRoutes, { prefix: '/accounts' })
   server.register(registerPaymentRoutes, { prefix: '/payments' })
   server.register(registerXenteWebhookRoutes, { prefix: '/webhooks' })
 
